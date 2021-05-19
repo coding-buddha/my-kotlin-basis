0dcde4421cb66fed4d14b73c7b1d9e89e622cdc4
pasudo123 <pasudo123@naver.com>
2021-05-19 18:40:19 +0900

    ws study

--- springboot-websocket-basis/README.md
+++ springboot-websocket-basis/README.md
@@ -2,7 +2,11 @@
 
 ## 준비
 * http://localhost:8080 을 실행한다.
-* `http://localhost:8080/index.html` 을 입력하여 웹소켓 수행을 한다.
+* `http://localhost:8080/index.html` 을 입력하여 대화형 웹소켓 수행을 한다.
+* `http://localhost:8080/index.html` 을 입력하여 스케줄링형 웹소켓 수행을 한다.
+
+## 개념
+* 
 
 ## reference
 * https://spring.io/guides/gs/messaging-stomp-websocket/
--- springboot-websocket-basis/src/main/kotlin/edu/pasudo123/study/demo/WebSocketConfiguration.kt
+++ springboot-websocket-basis/src/main/kotlin/edu/pasudo123/study/demo/WebSocketConfiguration.kt
@@ -17,16 +17,19 @@ class WebSocketConfiguration : WebSocketMessageBrokerConfigurer{
      * config.enableSimpleBroker 로 설정한 path 로 클라이언트에게 메시지를 전송한다.
      */
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
-        // 클리이언트가 /topic prefix 를 기준으로 서버에게 데이터를 받겠다는 의미
+        // 클리이언트가 "/topic" prefix 를 기준으로 서버에게 데이터를 받겠다는 의미
         config.enableSimpleBroker("/topic")
 
-        // 클라이언트에게 /app prefix 를 기준으로 서버에게 데이터를 보내겠다는 의미
+        // 클라이언트는 "/app" prefix 를 기준으로 서버에게 데이터를 보내겠다는 의미
         config.setApplicationDestinationPrefixes("/app")
     }
 
     /**
      * stomp single 프로토콜을 사용하기 위한 url
      * @MessageMapping 애노테이션에 매핑되는 EndPoint 를 설정한다.
+     *
+     * [1] 클라이언트는 /app/chat 으로 데이터를 전송하면, 서버는 @MessageMapping 에 매핑된 /chat 으로 데이터를 받는다.
+     * [2] 클라이언트는 /app/ws-task 으로 데이터를 전송하면, 서버는 @MessageMapping 에 매핑된 /chat 으로 데이터를 받는다.
      */
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/chat")
