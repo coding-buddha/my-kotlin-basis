0a8d60766588d679f0915fe22732a0be3fb7e711
pasudo123 <pasudo123@naver.com>
2021-05-25 23:14:54 +0900

    study

--- springboot-redis-basis/build.gradle.kts
+++ springboot-redis-basis/build.gradle.kts
@@ -7,14 +7,10 @@ dependencies {
     // web
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    // websocket
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
-    implementation("org.webjars:webjars-locator-core")
-    implementation("org.webjars:sockjs-client:1.0.2")
-    implementation("org.webjars:stomp-websocket:2.3.3")
-    implementation("org.webjars:bootstrap:3.3.7")
-    implementation("org.webjars:jquery:3.1.1-1")
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
+    // basic
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
