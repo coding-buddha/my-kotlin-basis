467f412c804429ca4e0486dbb8fa15d973079072
pasudo123 <pasudo123@naver.com>
2021-07-03 00:56:38 +0900

    fix objectmapper

--- springboot-concurrency-basis/src/main/kotlin/com/example/springbootconcurrencybasis/domain/config/ObjectMapperConfiguration.kt
+++ springboot-concurrency-basis/src/main/kotlin/com/example/springbootconcurrencybasis/domain/config/ObjectMapperConfiguration.kt
@@ -16,8 +16,10 @@ class ObjectMapperConfiguration {
 
         // @JsonProperty 가 없어도 디폴트 생성자가 없는 객체에 @RequestBody 클래스의 필드를 매핑할 수 있다.
         // https://proandroiddev.com/parsing-optional-values-with-jackson-and-kotlin-36f6f63868ef
-        objectMapper.findAndRegisterModules()
         objectMapper.registerModule(KotlinModule())
+
+        // LocalDateTime 에 대한 @JsonFormat 파싱을 가능하게 한다.
+        objectMapper.registerModule(JavaTimeModule())
         return objectMapper
     }
 }
